a30be3c82cd3fb10c686deb6270b29c932e39cc8
David Aspesi <davidnaspesi@gmail.com>
2018-05-14 14:59:14 -0600

    initial commit, app working

---
+++ accu_therm.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, request, render_template
+import random
+app = Flask(__name__)
+app.debug = True # Make this False if you are no longer debugging
+
+@app.route("/")
+def lab_temp():
+    import sys
+    temperature = random.uniform(71, 73)
+    if temperature is not None:
+        return render_template("lab_temp.html",temp=temperature)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
